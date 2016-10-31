d43e0a811129758ba924bc88e6d59e77640aae43
smeth194243 <smor7432@740s-pl-114.local>
2016-10-31 13:23:51 -0600

    Added PlayingCardDeck init

--- PlayingCardDeck.swift
+++ PlayingCardDeck.swift
@@ -15,6 +15,19 @@ class PlayingCardDeck : Deck
     override init()
     {
         super.init()
+        
+        for suit in PlayingCard.validSuits()
+        {
+            for var rank = 1; rank < PlayingCard.maxRank(); rank += 1
+            {
+              //  let tempCard = PlayingCard()
+               // tempCard.rank = rank
+               // tempCard.suit = suit
+                
+            let tempCard = PlayingCard(withRank: rank, ofSuit: suit)
+                cards.append(tempCard)
+            }
+        }
     }
     func shuffleDeck() -> Void
     {

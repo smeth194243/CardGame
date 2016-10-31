f6171d1fa233a382728659800c56ad01b2a4c76b
smeth194243 <smor7432@740s-pl-114.local>
2016-10-31 12:59:38 -0600

    Added the validRanks and validSuits methods.

--- Model/PlayingCard.swift
+++ Model/PlayingCard.swift
@@ -80,4 +80,23 @@ class PlayingCard : Card
         return description
     }
     
+    
+    //The class modifier makes it so the method is visible without an instance
+    //You would call it by ClassName.method()
+    //In this case PlayingCard.validRanks()
+    
+    class func validRanks() -> [String]
+    {
+        return ["??", "A","2","3","4","5","6","7","8","9","10","J","Q","K",]
+    }
+    
+    class func maxRank () -> Int{
+        return validRanks().count - 1
+    }
+    
+    class func validSuits() -> [String]
+    {
+        return ["♦️","♠️","♥️","♣️"]
+    }
+    
 }

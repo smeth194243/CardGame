82c5d99d91fa554274ba664062485bc1807a3ec2
smeth194243 <smor7432@740s-pl-114.local>
2016-10-31 12:33:57 -0600

    Added Deck and subclass PlayingCardDeck

---
+++ Deck.swift
@@ -0,0 +1,14 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Morris, Seth on 10/31/16.
+//  Copyright © 2016 Morris, Seth. All rights reserved.
+//
+
+import Foundation
+
+class Deck
+{
+    
+}
---
+++ PlayingCardDeck.swift
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.swift
+//  CardGame
+//
+//  Created by Morris, Seth on 10/31/16.
+//  Copyright © 2016 Morris, Seth. All rights reserved.
+//
+
+import Foundation
+
+class PlayingCardDeck : Deck
+{
+    
+}

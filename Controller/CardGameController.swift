8aadc04eb89d639080ececb519f72221b6f77d17
smeth194243 <smor7432@740s-pl-114.local>
2016-11-07 13:26:23 -0700

    Did some things

--- Controller/CardGameController.swift
+++ Controller/CardGameController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class CardGameController : UIViewController
 {
+    private lazy var clickCount = Int()
+    
+    @IBOutlet weak var cardLabel: UILabel!
+    @IBOutlet weak var cardButton: UIButton!
+    
     override func viewDidLoad() -> Void
     {
         let tempCard = Card()
         print(tempCard.toString())
+    }
+    
+    
+    @IBAction func cardClick(sender: UIButton)
+    {
+        let content = "You clicked \(clickCount) times"
+        cardLabel.text = content
     }
     
 }

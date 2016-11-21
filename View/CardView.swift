db8f094462a0e1d87f3e791ca0dd9ab20d39956c
smeth194243 <smor7432@740s-pl-114.local>
2016-11-21 13:52:28 -0700

    Fixed CardView by adding required inits

--- View/CardView.swift
+++ View/CardView.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-@IBDesignable class CardView
+@IBDesignable class CardView : UIView
 {
     internal var frontImage : UIImageView
     internal var backImage : UIImageView
-    internal var isFaceUp : Bool
+    @IBInspectable internal var isFaceUp : Bool
+    @IBInspectable internal var cornerCurve : CGFloat
     internal var bottomLabel : UILabel
     internal var topLabel : UILabel
     
@@ -23,5 +24,23 @@ import UIKit
         isFaceUp = Bool()
         bottomLabel = UILabel()
         topLabel = UILabel()
+        cornerCurve = CGFloat()
+        super.init(frame: CGRect(x:0,y:0,width:400, height:800))
+    }
+    
+    required init?(coder acoder:NSCoder)
+    {
+        frontImage = UIImageView()
+        backImage = UIImageView()
+        isFaceUp = Bool()
+        bottomLabel = UILabel()
+        topLabel = UILabel()
+        cornerCurve = CGFloat()
+        super.init(coder: acoder)
+    }
+    
+    func setup() -> Void
+    {
+        
     }
 }
